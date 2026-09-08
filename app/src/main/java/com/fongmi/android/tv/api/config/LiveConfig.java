113b56201af41877f898810bec917b7d579c7aa0
15840213978 <105612887+15840213978@users.noreply.github.com>
2026-09-08 19:01:43 +0800

    Update LiveConfig.java

--- app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
@@ -129,9 +129,16 @@ public class LiveConfig extends BaseConfig {
             initLive(config, new JsonObject());
             return;
         }
-        String json = Decoder.getJson(UrlUtil.convert(config.getUrl()), TAG);
-        if (Json.isObj(json)) checkJson(config, Json.parse(json).getAsJsonObject());
-        else parseText(config, json);
+    String source = config.getUrl();
+
+    if (TextUtils.isEmpty(source)) {
+    source = DEFAULT_CLYS;
+    }
+
+    String json = Decoder.getJson(UrlUtil.convert(source), TAG);
+
+    if (Json.isObj(json)) checkJson(config, Json.parse(json).getAsJsonObject());
+    else parseText(config, json);    
     }
 
     @Override
